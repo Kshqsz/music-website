2dabb449235a2a6f2d3b11f790babf9643d77ea3
hanshoukun <2456480538@qq.com>
2024-07-02 23:33:40 +0800

    首次提交, 初始化项目

---
+++ music-backend/src/test/java/cd/edu/usts/cs2022/MusicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cd.edu.usts.cs2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
