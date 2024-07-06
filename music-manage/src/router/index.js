c2c0c60a363f10abda503b4623d623930c92f450
hanshoukun <2456480538@qq.com>
2024-07-06 18:29:09 +0800

    同步远程仓库

--- music-manage/src/router/index.js
+++ music-manage/src/router/index.js
@@ -1,23 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import login from '@/views/LoginIndex.vue'
+import layout from '@/views/LayoutIndex.vue'
+import singer from '@/views/SingerIndex.vue'
+import song from '@/views/SongIndex.vue'
+import user from '@/views/UserIndex.vue'
+import home from '@/views/HomeIndex.vue'
+
+
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'login',
+    component: login
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/layout',
+    name: 'home',
+    component: layout,
+    children: [
+      {path: '/home', component: home},
+      {path: '/singer', component: singer},
+      {path: '/song' , component: song},
+      {path: '/user', component: user}
+    ]
   }
+ 
 ]
 
 const router = new VueRouter({
---
+++ music-manage/src/views/LayoutIndex.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <el-col :span="3">
+    <el-menu default-active="/home" class="el-menu-vertical-demo" router="true">
+      <el-menu-item index="/home" router="true" >
+        <span slot="title">首页</span>
+        </el-menu-item>
+
+      <el-menu-item index="/song" router="true">
+        <span slot="title">歌单管理</span>
+      </el-menu-item>
+
+      <el-menu-item index="/singer" router="true">
+        <span slot="title">歌手管理</span>
+      </el-menu-item>
+
+      <el-menu-item index="user" router="true">
+        <span slot="title">用户管理</span>
+      </el-menu-item>
+    </el-menu>
+  </el-col> 
+   <router-view ></router-view>
+  </div>
+  
+</template>
+
+<script>
+
+export default({
+  setup() {
+    
+  },
+})
+</script>
